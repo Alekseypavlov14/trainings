05c807c12dba5e830e834de2a62db33522ac2487
Alekseypavlov14 <aleshapavlov14@gmail.com>
2022-12-21 21:48:43 +0200

    added new Footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -6,7 +6,7 @@ interface FooterProps {}
 export const Footer: FC<FooterProps> = () => {
   return (
     <div className={styles.Footer}>
-      
+      &copy; Aleksey Pavlov 2022
     </div>
   )
 }
