0fb8a5483d53c9b3a16554a3f056dc43009032f5
Alekseypavlov14 <aleshapavlov9@gmail.com>
2022-12-29 12:02:35 +0200

    selected colors for Day

--- src/features/calendar/components/Day/Day.tsx
+++ src/features/calendar/components/Day/Day.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react'
 import { focusDate, focusedDateSelector } from '@features/calendar/calendar.slice'
 import { useDispatch, useSelector } from 'react-redux'
 import { areDatesEqual } from '@features/calendar/utils/areDatesEqual'
+import { useActiveDays } from '@features/history/hooks/useActiveDays'
 import { parseDate } from '@features/calendar/utils/parseDate'
 import type { Date } from '@features/calendar/types/Date'
 import styles from './Day.module.css'
@@ -14,10 +15,18 @@ interface DayProps {
 export const Day: FC<DayProps> = ({ date }) => {
   const dispatch = useDispatch()
   const focusedDate = useSelector(focusedDateSelector)
+  const activeDates = useActiveDays().map(day => parseDate(day))
 
   const isToday = areDatesEqual(date, parseDate(Date.now()))
   const isFocused = areDatesEqual(date, focusedDate)
-  const classNames = cn(styles.Day, isToday && styles.Today, isFocused && styles.Focused)
+  const isActiveDate = activeDates.some(activeDate => areDatesEqual(activeDate, date))
+
+  const classNames = cn(
+    styles.Day, 
+    isToday && styles.Today, 
+    isFocused && styles.Focused, 
+    isActiveDate && styles.ActiveDate
+  )
 
   const focusDateHandler = () => dispatch(focusDate({ date }))
 
