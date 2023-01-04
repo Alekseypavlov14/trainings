c5953021136ae53afc00e985042fee082bbcf86f
Alekseypavlov14 <aleshapavlov9@gmail.com>
2023-01-04 12:19:57 +0200

    updated ColorHelper realization

--- src/features/calendar/components/ColorHelper/ColorHelper.tsx
+++ src/features/calendar/components/ColorHelper/ColorHelper.tsx
@@ -2,7 +2,6 @@ import { FC } from 'react'
 import { Modal } from 'standard-ui'
 import { ColorMark, Colors } from '@features/calendar/components/ColorMark/ColorMark'
 import styles from './ColorHelper.module.css'
-import cn from 'classnames'
 
 interface ColorHelperProps {
   isShown: boolean
@@ -10,29 +9,27 @@ interface ColorHelperProps {
 }
 
 export const ColorHelper: FC<ColorHelperProps> = ({ isShown, onClose }) => {
-  const classNames = cn(!isShown && styles.Hidden)
 
   return (
-    <div className={classNames}>
-      <Modal 
-        title='Color Palette' 
-        onClose={onClose}
-      >
-        <>
-          <div className={styles.Row}>
-            <ColorMark color={Colors.Green} />
-            <div className={styles.ColorMeaning}>Days when you were training</div>
-          </div>
-          <div className={styles.Row}>
-            <ColorMark color={Colors.Blue} />
-            <div className={styles.ColorMeaning}>Day which you selected to check trainings</div>
-          </div>
-          <div className={styles.Row}>
-            <ColorMark color={Colors.Yellow} />
-            <div className={styles.ColorMeaning}>Days when you did that exercise you selected to check</div>
-          </div>
-        </>
-      </Modal>
-    </div>
+    <Modal 
+      title='Color Palette' 
+      onClose={onClose}
+      isShown={isShown}
+    >
+      <>
+        <div className={styles.Row}>
+          <ColorMark color={Colors.Green} />
+          <div className={styles.ColorMeaning}>Days when you were training</div>
+        </div>
+        <div className={styles.Row}>
+          <ColorMark color={Colors.Blue} />
+          <div className={styles.ColorMeaning}>Day which you selected to check trainings</div>
+        </div>
+        <div className={styles.Row}>
+          <ColorMark color={Colors.Yellow} />
+          <div className={styles.ColorMeaning}>Days when you did that exercise you selected to check</div>
+        </div>
+      </>
+    </Modal>
   )
 }
